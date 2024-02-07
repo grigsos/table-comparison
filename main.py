379bb6e8b05f3039adf3290bb86bbbb90f35b851
Grigorii Osipov <grigorii.osipov@dataminded.com>
2024-02-07 15:17:53 +0100

    fix(main): correcting a mismatch of rows #

--- main.py
+++ main.py
@@ -6,31 +6,21 @@ def remove_columns(df, columns_to_remove):
     return df.drop(columns=columns_to_remove, errors='ignore')
 
 def compare_dataframes(df1, df2, unique_key):
-    """
-    Compare two DataFrames by a unique key and return the differences.
 
-    Parameters:
-    - df1: The first DataFrame.
-    - df2: The second DataFrame.
-    - unique_key: The column name to use as the unique identifier for comparison.
-
-    Returns:
-    - A DataFrame containing the differences.
-    """
     # Ensure the unique_key column is present in both DataFrames
     if unique_key not in df1.columns or unique_key not in df2.columns:
         raise ValueError(f"Unique key '{unique_key}' not found in one of the DataFrames.")
     
     if df1[unique_key].nunique() != len(df1) or df2[unique_key].nunique() != len(df2):
         raise ValueError(f"The unique key '{unique_key}' is not unique the logged comparison before this error.")
-
+    
+    if len(df1) != len(df2):
+        raise ValueError(f"Lengths of the two dataframes are different: {len(df1)} and {len(df2)}")
     # Reorder the DataFrames by the unique key to ensure row alignment
     df1_sorted = df1.sort_values(by=unique_key).reset_index(drop=True)
     df2_sorted = df2.sort_values(by=unique_key).reset_index(drop=True)
 
-    # if unique_key == 'derived_project_name_hash':
-    #     print(df1_sorted.head())
-    #     print(df2_sorted.head())
+    
 
     # Compare the sorted DataFrames and get the rows that differ
     df_diff = df1_sorted.compare(df2_sorted)
@@ -46,28 +36,38 @@ for filename in os.listdir(initial_data_folder):
         base_filename = filename[:-16]
 
         initial_df_path = os.path.join(initial_data_folder, filename)
-        initial_df = pd.read_csv(initial_df_path)
+        # read csv which can also have multiline data
+        initial_df = pd.read_csv(initial_df_path, quotechar='"', escapechar="\\")
         initial_df = remove_columns(initial_df, ['derived_inserted_at'])
         
         for post_filename in os.listdir(post_data_folder):
             if post_filename.startswith(base_filename) and post_filename.endswith('.csv'):
                 post_df_path = os.path.join(post_data_folder, post_filename)
-                post_df = pd.read_csv(post_df_path)
+                post_df = pd.read_csv(post_df_path, quotechar='"', escapechar="\\")
                 post_df = remove_columns(post_df, ['derived_inserted_at'])
 
                 if 'technology_schedule' in filename:
                     unique_key = 'taskid'
                     post_df = remove_columns(post_df, ['index'])
-                    initial_df = remove_columns(post_df, ['index'])
+                    initial_df = remove_columns(initial_df, ['index'])
                 elif 'early_pipeline' in filename:
                     unique_key = 'derived_project_name_hash'
                     post_df = remove_columns(post_df, ['index'])
-                    initial_df = remove_columns(post_df, ['index'])
+                    initial_df = remove_columns(initial_df, ['index'])
                 elif 'pipeline' in filename:
+                    print("pipeline")
                     unique_key = 'derived_trial_name_hash'
                     post_df = remove_columns(post_df, ['index'])
-                    initial_df = remove_columns(post_df, ['index'])
-                
+                    initial_df = remove_columns(initial_df, ['index'])
+                elif 'charter' in filename:
+                    unique_key = 'uniqueid'
+                    post_df = remove_columns(post_df, ['index'])
+                    initial_df = remove_columns(initial_df, ['index'])
+                elif 'scope' in filename:
+                    unique_key = 'uniqueid'
+                    post_df = remove_columns(post_df, ['index'])
+                    initial_df = remove_columns(initial_df, ['index'])
+
                 else:
                     unique_key = 'index'
 
